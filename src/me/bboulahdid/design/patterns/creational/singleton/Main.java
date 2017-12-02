556984615ed9b4b8036254ff090f536c8ef8f839
Boubker Boulahdid <boulahdid.boubker@gmail.com>
2017-12-02 22:11:51 +0000

    Update SingletonEnum.java

--- src/me/bboulahdid/design/patterns/creational/singleton/Main.java
+++ src/me/bboulahdid/design/patterns/creational/singleton/Main.java
@@ -3,15 +3,19 @@ package me.bboulahdid.design.patterns.creational.singleton;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Singleton singleton1 = Singleton.getThreadSafeInstance();
 		Singleton singleton2 = Singleton.getThreadSafeInstance();
-		
+
 		System.out.println(singleton1);
 		System.out.println(singleton2);
-		
+
 		System.out.println(singleton1 == singleton2);
-		
+
+
+		System.out.println("# Using enums :");
+		SingletonEnum.INSTANCE.setValue(10);
+		System.out.println(SingletonEnum.INSTANCE.getValue());
 	}
 
 }
--- src/me/bboulahdid/design/patterns/creational/singleton/SingletonEnum.java
+++ src/me/bboulahdid/design/patterns/creational/singleton/SingletonEnum.java
@@ -1,5 +1,21 @@
 package me.bboulahdid.design.patterns.creational.singleton;
 
+/**
+ *
+ * A great article about singleton design pattern best practices :
+ * https://www.journaldev.com/1377/java-singleton-design-pattern-best-practices-examples
+ *
+ */
 public enum SingletonEnum {
+	INSTANCE;
 
+	private int value;
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
 }
